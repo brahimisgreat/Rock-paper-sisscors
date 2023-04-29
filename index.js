7b1b107c1b0543f875e7a79470418e5d47596d09
BrahimIsGreat Brahim coulibaly <brahimjobs@gmail.com>
2023-04-29 18:17:29 -0400

    yes

--- index.js
+++ index.js
@@ -4,5 +4,101 @@ when we click a button
 2.compare the moves to get the result
 3.Display the result in a popup
 */
+function Rock() {
+const randomNumber = Math.random();
+        let computerMove = ''
+          if (randomNumber >= 0 && randomNumber <= 1/3){
+             computerMove = 'rock'
+             
+        }
+        else if (randomNumber >= 1/3 && randomNumber <= 2/3){
+            computerMove = 'paper'
+            
+        }
+        else if (randomNumber >= 2/3 && randomNumber <= 1){
+            computerMove = 'scissors'
+           
+        }
+            console.log(computerMove)
+           
 
-onclick 
+
+            let results = '';
+
+        if (computerMove === 'rock'){
+            results = 'tie'
+        }   else if (computerMove === 'paper'){
+            results = 'you lose';
+        } else if (computerMove === 'scissors'){
+            results = 'you win'
+        }
+        alert(`you picked rock. Computer picked ${computerMove}. ${results}`)
+    }
+
+    function paper() {
+        const randomNumber = Math.random();
+                let computerMove = ''
+                  if (randomNumber >= 0 && randomNumber <= 1/3){
+                     computerMove = 'rock'
+                     
+                }
+                else if (randomNumber >= 1/3 && randomNumber <= 2/3){
+                    computerMove = 'paper'
+                    
+                }
+                else if (randomNumber >= 2/3 && randomNumber <= 1){
+                    computerMove = 'scissors'   
+                };
+
+                    console.log(computerMove)
+                   
+        
+        
+                let results = '';
+        
+                if (computerMove === 'rock'){
+                    results = 'you win'
+                }   else if (computerMove === 'paper'){
+                    results = 'tie';
+                } else if (computerMove === 'scissors'){
+                    results = 'you lose'
+                };
+
+                alert(`you picked paper. Computer picked ${computerMove}. ${results}`);
+            }
+
+
+            function scissors() {
+                const randomNumber = Math.random();
+                        let computerMove = ''
+                          if (randomNumber >= 0 && randomNumber <= 1/3){
+                             computerMove = 'rock'
+                             
+                        }
+                        else if (randomNumber >= 1/3 && randomNumber <= 2/3){
+                            computerMove = 'paper'
+                            
+                        }
+                        else if (randomNumber >= 2/3 && randomNumber <= 1){
+                            computerMove = 'scissors'   
+                        };
+        
+                            console.log(computerMove)
+                           
+                
+                
+                        let results = '';
+                
+                        if (computerMove === 'rock'){
+                            results = 'you lose'
+                        }   else if (computerMove === 'paper'){
+                            results = 'you win';
+                        } else if (computerMove === 'scissors'){
+                            results = 'tie'
+                        };
+        
+                        alert(`you picked scissors. Computer picked ${computerMove}. ${results}`);
+                    }
+
+
+           
